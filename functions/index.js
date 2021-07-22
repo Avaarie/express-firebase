e752739d95400ad89afb69b55a0a85f081a37156
Oliwia <legendhm@gmail.com>
2021-07-22 14:08:29 +0100

    FT - No idea how and wy but it's working, firebase deployed

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
+const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const PORT = 3000;
+const app = express();
+
+app.get("/hello", (req, res, next)=>{
+  res.send("Welcome to FIRIEIEE");
+});
+app.listen(PORT, ()=>{
+  console.log("Server is running on PORT", PORT);
+});
+exports.app = functions.https.onRequest(app);
